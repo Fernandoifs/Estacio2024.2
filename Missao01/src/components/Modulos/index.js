e2a7099f2e5c1ebcc2d840fad4c03f0146748f3c
Fernandoifs <129119566+Fernandoifs@users.noreply.github.com>
2024-09-20 18:19:18 -0300

    remoção do modulos, não será utilizado;

--- Missao01/src/components/Modulos/index.js
+++
@@ -1,9 +0,0 @@
-export default class Modulos {
-    constructor(id, title, color, icon, screenName) {
-        this.id = id;
-        this.title = title;
-        this.color = color;
-        this.icon = icon;
-        this.screenName = screenName;
-    }
-}
